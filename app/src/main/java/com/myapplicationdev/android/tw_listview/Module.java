bba02a7971d2debca5b894fbb9a0f2bf18778794
16022603 <16022603@myrp.edu.sg>
2018-04-24 13:38:47 +0800

    first screen

---
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/Module.java
@@ -0,0 +1,24 @@
+package com.myapplicationdev.android.tw_listview;
+
+public class Module {
+    private String module;
+    private String year;
+    private boolean program;
+    public Module(String year, String module, boolean program){
+        this.year = year;
+        this.module = module;
+        this.program = program;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getModule(){
+        return module;
+    }
+
+    public boolean isProgram() {
+        return program;
+    }
+}
--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -3,13 +3,18 @@ package com.myapplicationdev.android.tw_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     TextView tvYear;
+    ArrayList<Module> modules;
+    ArrayAdapter aa;
 
 
     @Override
@@ -24,7 +29,29 @@ public class SecondActivity extends AppCompatActivity {
         String year = i.getStringExtra("year");
         tvYear.setText(year);
 
+        modules = new ArrayList<Module>();
+        modules.add(new Module("Year 1", "A113", false));
+        modules.add(new Module("Year 1", "B102", false));
+        modules.add(new Module("Year 1", "C207", true));
 
+        modules.add(new Module("Year 2", "C208", true));
+        modules.add(new Module("Year 2", "C200", false));
+        modules.add(new Module("Year 2", "C346", true));
+
+        modules.add(new Module("Year 3", "C347", true));
+        modules.add(new Module("Year 3", "C302", true));
+        modules.add(new Module("Year 3", "C349", true));
+
+        if(tvYear.equals("Year 1")){
+            aa = new ModuleAdapter(this, R.layout.row2, modules);
+            lv.setAdapter(aa);
+        } else if(tvYear.equals("Year 2")){
+            aa = new ModuleAdapter(this, R.layout.row2, modules);
+            lv.setAdapter(aa);
+        }else{
+            aa = new ModuleAdapter(this, R.layout.row2, modules);
+            lv.setAdapter(aa);
+        }
 
     }
 
