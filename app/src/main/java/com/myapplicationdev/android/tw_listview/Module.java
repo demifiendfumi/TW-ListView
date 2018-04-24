84efe642a9819e148cc7a2f6eae3f9f544ae1f5b
16023018 <16023018@rp.edu.sg>
2018-04-24 13:54:38 +0800

    Latest update as of 1354

--- app/src/main/java/com/myapplicationdev/android/tw_listview/Module.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/Module.java
@@ -1,24 +1,8 @@
 package com.myapplicationdev.android.tw_listview;
 
 public class Module {
-<<<<<<< HEAD
-    private String name;
-    private boolean star;
-    private String year;
-    public Module(String year, String name, boolean star) {
-        this.name = name;
-        this.star = star;
-    }
-    public String getName() {
-        return name;
-    }
-    public boolean isStar() {
-        return star;
-    }
-    public String getYear() {
-        return year;
-    }
-=======
+
+
     private String module;
     private String year;
     private boolean program;
@@ -39,5 +23,4 @@ public class Module {
     public boolean isProgram() {
         return program;
     }
->>>>>>> origin/master
 }
